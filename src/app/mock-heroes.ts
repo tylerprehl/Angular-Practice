8da775e2f7bf69c5228ca83ab9e9735394a6681b
Tyler Prehl <tyler@prehl.us>
2023-03-23 23:28:58 -0400

    sections 1-4 complete

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Mr. Incredible' },
+  { id: 13, name: 'Dash' },
+  { id: 14, name: 'Paul Rudd' },
+  { id: 15, name: 'Dr. Strange' },
+  { id: 16, name: 'Peter Quill' },
+  { id: 17, name: 'Mike Wazowski' },
+  { id: 18, name: 'Pnut' },
+  { id: 19, name: 'Miles Morales' },
+  { id: 20, name: 'T Prehl' }
+]
